b4ab86327fd438481cf9d1ca7134f7b5276ca22f
Ashutosh Patil <patilashutosh67@gmail.com>
2023-08-24 01:11:43 +0530

    leapyears, tempConvert done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  answer = (fahrenheit-32)*5/9
+  finalConversionToCelcius= Math.round(answer*10)/10;
+  return finalConversionToCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  answer= (celcius* 9/5 +32);
+  finalConversionToFahrenheit= Math.round(answer*10)/10;
+  return finalConversionToFahrenheit;
 };
 
 // Do not edit below this line
@@ -9,3 +15,4 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
