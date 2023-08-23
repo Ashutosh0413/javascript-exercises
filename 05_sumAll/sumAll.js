85bf76a3a78b5b0ac129aaf91c8b7645c844478b
Ashutosh Patil <patilashutosh67@gmail.com>
2023-08-24 00:32:28 +0530

    sum of all elements betweeen two numbers done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+// should do swap of num1,num2 for more better soln
+    finalSum = 0
+    if (num1<num2 && num1 > 0 && num2 > 0 && typeof num1 === 'number' && typeof num2 === 'number'){
+        for(i=num1;i<=num2;i++){
+            finalSum += i
+        }
+    }else if(num1 > num2 && num1 > 0 && num2 > 0 && typeof num1 === 'number' && typeof num2 === 'number'){
+        for(i=num1;i>=num2;i--){
+            finalSum += i
+        }
+    }else{
+        return 'ERROR'
+    }
+    return finalSum;
+
+
 
 };
 
